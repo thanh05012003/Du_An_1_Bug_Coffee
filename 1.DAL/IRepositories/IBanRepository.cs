b9da111d8cae42ff56c47f1e27f1a4ec9967a388
thanh05012003 <0395358670b@gmail.com>
2022-11-17 12:40:27 +0700

    Signed-off-by: thanh05012003 <0395358670b@gmail.com>

---
+++ 1.DAL/IRepositories/IBanRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1.DAL.DomainClass;
+
+namespace _1.DAL.IRepositories
+{
+    public interface IBanRepository
+    {
+        bool add(Ban obj);
+        bool delete(Ban obj);
+        bool udpate(Ban obj);
+        List<Ban> GetAll();
+    }
+}
---
+++ 1.DAL/Repositories/BanRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1.DAL.DomainClass;
+using _1.DAL.IRepositories;
+
+namespace _1.DAL.Repositories
+{
+    public class BanRepository:IBanRepository
+
+    {
+        public bool add(Ban obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool delete(Ban obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool udpate(Ban obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Ban> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
