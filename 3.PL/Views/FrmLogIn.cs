c7a9a3f737c79c2c35dd4622f17784990d0ffbed
thanh05012003 <0395358670b@gmail.com>
2022-11-22 14:20:17 +0700

    update login

--- 3.PL/Views/FrmLogIn.cs
+++ 3.PL/Views/FrmLogIn.cs
@@ -10,48 +10,11 @@ using System.Windows.Forms;
 
 namespace _3.PL.Views
 {
-    public partial class FrmLogIn : Form
+    public partial class FrmLogin : Form
     {
-        public FrmLogIn()
+        public FrmLogin()
         {
             InitializeComponent();
-            lb_EmailFail.Visible = false;
-            lb_MatKhauFail.Visible = false;
-        }
-
-        public void CheckEmail()
-        {
-            if (txt_Email.Text.Trim() == "")
-            {
-                lb_EmailFail.Visible = true;
-                lb_EmailFail.Text = "Không được để trống email";
-                
-            }
-
-        }
-        public void CheckMatKhau()
-        {
-            if (txt_MatKhau.Text.Trim() == "")
-            {
-                lb_MatKhauFail.Visible = true;
-                lb_MatKhauFail.Text = "Không được để trống mật khẩu";
-            }
-        }
-
-        private void btn_DangNhap_Click(object sender, EventArgs e)
-        {
-           CheckEmail();
-           CheckMatKhau();
-        }
-
-        private void txt_Email_TextChanged(object sender, EventArgs e)
-        {
-            lb_EmailFail.Visible = false;
-        }
-
-        private void txt_MatKhau_TextChanged(object sender, EventArgs e)
-        {
-            lb_MatKhauFail.Visible = false;
         }
     }
 }
