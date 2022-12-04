37c92212b0b004c339c1ee30a146f583eb8a8d65
thanh05012003 <92389586+thanh05012003@users.noreply.github.com>
2022-12-04 22:55:30 +0700

    update frmkhuyenmai

--- 3.PL/Program.cs
+++ 3.PL/Program.cs
@@ -21,7 +21,7 @@ namespace _3.PL
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           Application.Run(new FrmLoaiSP()); 
+           Application.Run(new FrmLogin()); 
       
         }
     }
